70abf3c89e123ea629ffe6ab86f5f072fc689191
Kiyoto Tanaka <tanaka.kiyoto56@gmail.com>
2026-03-01 19:37:46 +0900

    initial commit

---
+++ src/main/java/com/github/tanakakfuji/vending_machine_api/VendingMachineApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.tanakakfuji.vending_machine_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendingMachineApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendingMachineApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/tanakakfuji/vending_machine_api/VendingMachineApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.tanakakfuji.vending_machine_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendingMachineApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
