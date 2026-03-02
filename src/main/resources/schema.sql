ecfe9f19f88fe4a7a924e8f2c8d8eed8c7256604
Kiyoto Tanaka <tanaka.kiyoto56@gmail.com>
2026-03-02 09:39:10 +0900

    DBテーブルを定義

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,18 @@
+CREATE TABLE if NOT EXISTS vending_machine (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL UNIQUE,
+    item_capacity INTEGER NOT NULL CHECK (item_capacity >= 0),
+    status VARCHAR(255) DEFAULT 'CLOSED' CHECK (status IN ('OPEN', 'CLOSED')) NOT NULL
+);
+
+CREATE TABLE if NOT EXISTS drink (
+    id SERIAL PRIMARY KEY,
+    vm_id INTEGER NOT NULL,
+    name VARCHAR(255) NOT NULL UNIQUE,
+    volume INTEGER NOT NULL CHECK (volume > 0),
+    price INTEGER NOT NULL CHECK (price >= 0),
+    stock INTEGER NOT NULL DEFAULT 0 CHECK (stock >= 0),
+
+    CONSTRAINT fk_vm FOREIGN KEY (vm_id)
+    REFERENCES vending_machine(id) ON DELETE CASCADE
+);
