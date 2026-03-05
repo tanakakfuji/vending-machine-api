53f6ecdcd1596fd989d3fb52d8d9e7ee765d5be7
Kiyoto Tanaka <tanaka.kiyoto56@gmail.com>
2026-03-05 22:35:40 +0900

    drinkテーブルのvm_id列とname列に複合ユニーク制約を付与

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -8,11 +8,23 @@ CREATE TABLE if NOT EXISTS vending_machine (
 CREATE TABLE if NOT EXISTS drink (
     id SERIAL PRIMARY KEY,
     vm_id INTEGER NOT NULL,
-    name VARCHAR(255) NOT NULL UNIQUE,
+    name
+    VARCHAR
+(
+    255
+) NOT NULL,
     volume INTEGER NOT NULL CHECK (volume > 0),
     price INTEGER NOT NULL CHECK (price >= 0),
     stock INTEGER NOT NULL DEFAULT 0 CHECK (stock >= 0),
 
     CONSTRAINT fk_vm FOREIGN KEY (vm_id)
-    REFERENCES vending_machine(id) ON DELETE CASCADE
+    REFERENCES vending_machine
+(
+    id
+) ON DELETE CASCADE,
+    CONSTRAINT unique_vm_drink_name UNIQUE
+(
+    vm_id,
+    name
+)
 );
