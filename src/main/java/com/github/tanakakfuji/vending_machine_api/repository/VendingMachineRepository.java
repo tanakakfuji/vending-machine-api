ae32f200adb49d18a8f9d88fcdfdd4ed436e6d80
Kiyoto Tanaka <tanaka.kiyoto56@gmail.com>
2026-03-07 12:34:31 +0900

    自販機の名前の重複をDBに問い合わせるメソッドを追加

--- src/main/java/com/github/tanakakfuji/vending_machine_api/repository/VendingMachineRepository.java
+++ src/main/java/com/github/tanakakfuji/vending_machine_api/repository/VendingMachineRepository.java
@@ -1,7 +1,11 @@
 package com.github.tanakakfuji.vending_machine_api.repository;
 
+import com.github.tanakakfuji.vending_machine_api.domain.model.vm.Name;
 import com.github.tanakakfuji.vending_machine_api.domain.model.vm.VendingMachine;
 import org.springframework.data.repository.ListCrudRepository;
 
 public interface VendingMachineRepository extends ListCrudRepository<VendingMachine, Integer> {
+    boolean existsByName(Name name);
+
+    boolean existsByNameAndIdNot(Name name, Integer id);
 }
