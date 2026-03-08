446f82c3e1a00649ad7f97d5d4018f7dc8605f5a
Kiyoto Tanaka <tanaka.kiyoto56@gmail.com>
2026-03-08 12:02:12 +0900

    ステータスを指定してレコードを取得するメソッドを追加

--- src/main/java/com/github/tanakakfuji/vending_machine_api/repository/VendingMachineRepository.java
+++ src/main/java/com/github/tanakakfuji/vending_machine_api/repository/VendingMachineRepository.java
@@ -1,11 +1,15 @@
 package com.github.tanakakfuji.vending_machine_api.repository;
 
 import com.github.tanakakfuji.vending_machine_api.domain.model.vm.Name;
+import com.github.tanakakfuji.vending_machine_api.domain.model.vm.Status;
 import com.github.tanakakfuji.vending_machine_api.domain.model.vm.VendingMachine;
 import org.springframework.data.repository.ListCrudRepository;
 
+import java.util.List;
+
 public interface VendingMachineRepository extends ListCrudRepository<VendingMachine, Integer> {
     boolean existsByName(Name name);
-
     boolean existsByNameAndIdNot(Name name, Integer id);
+
+    List<VendingMachine> findByStatus(Status status);
 }
